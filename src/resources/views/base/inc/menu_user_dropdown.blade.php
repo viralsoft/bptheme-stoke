fe906c92235946b5ad0531f6e469dba12c86e53d
“ViralS-longnd” <longnd@viralsoft.vn>
2021-03-17 17:42:51 +0700

    update auth

--- src/resources/views/base/inc/menu_user_dropdown.blade.php
+++ src/resources/views/base/inc/menu_user_dropdown.blade.php
@@ -2,7 +2,7 @@
     <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <img class="rounded-circle header-profile-user" src="{{ backpack_avatar_url(backpack_auth()->user()) }}" alt="{{ backpack_auth()->user()->name }}">
-        <span class="d-none d-xl-inline-block ms-1" key="t-henry">{{ucfirst(@backpack()->user()->name)}}</span>
+        <span class="d-none d-xl-inline-block ms-1" key="t-henry">{{ucfirst(@backpack_auth()->user()->name)}}</span>
         <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
     </button>
     <div class="dropdown-menu dropdown-menu-end">
